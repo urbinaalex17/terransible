b46b64fc1b73c0e8aae9096780b7aa50168a47be
urbinaalex17 <w.alexanderuc@gmail.com>
2018-11-01 19:16:43 +0000

    AMI in main.tf

--- main.tf
+++ main.tf
@@ -435,3 +435,25 @@ EOD
     command = "aws ec2 wait instance-status-ok --instance-ids ${aws_instance.wp_ec2_dev.id} --profile ${var.aws_profile} && ansible-playbook -i aws_hosts wordpress.yml"
   }
 }
+
+#------- GOLDEN AMI -----------
+
+resource "random_id" "golden_ami" {
+  byte_length = 3
+}
+
+resource "aws_ami_from_instance" "aws_golden_ami" {
+  name               = "wp-ami-${random_id.golden_ami.b64}"
+  source_instance_id = "${aws_instance.wp_ec2_dev.id}"
+
+  provisioner "local-exec" {
+    command = <<EOT
+cat <<EOF > userdata
+#!/bin/bash
+/usr/bin/aws s3 sync s3://${aws_s3_bucket.code.bucket} /var/www/html/
+/bin/touch /var/spool/cron/root
+sudo /bin/echo '*/5 * * * * aws s3 sync s3://${aws_s3_bucket.code.bucket} /var/www/html' >> /var/spool/cron/root
+EOF
+EOT
+  }
+}
